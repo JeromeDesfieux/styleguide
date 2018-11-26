7a59f82abf0463d8714ff8fcb11ccf1d98b310a3
Jérôme DESFIEUX <jerome.desfieux@navblue.aero>
2018-11-26 09:34:23 +0100

    First version of Mission+ coding rules

---
+++ MyClass.cpp
@@ -0,0 +1,24 @@
+/*
+* Copyright 2019 NAVBLUE. All rights reserved.
+*/
+
+/********* PROJECTS INCLUDES ************/
+#include "MyClass.h"
+
+/********* COTS INCLUDES ************/
+#include "OtherClass.h"
+
+using namespace Navblue::MissionPlus;
+
+MyClass::MyClass() {
+
+}
+
+double MyClass::oneUsefulMethod(const OtherClass& argWellNamed) {
+
+}
+
+int MyClass::onePrivateMethod(int nbWaypoints) {
+    int localVariable;
+
+}
---
+++ MyClass.h
@@ -0,0 +1,56 @@
+/*
+* Copyright 2019 NAVBLUE. All rights reserved.
+*/
+#pragma once
+
+namespace Navblue {
+namespace MissionPlus {
+  class OtherClass;
+
+/**
+* \class ARealyNiceClass
+* \brief A really nice class demonstrating our code guidelines
+*/
+class MyClass {
+   public:
+    // CTors & DTor
+
+    /** \brief constructor */
+    MyClass();
+    /** \brief destructor */
+    ~MyClass() = default;
+
+    // Methods
+
+    /**
+    * \brief A very ususeful method doing a lot of thing like this and that
+    * \param[in] argWellNamed is a parameter that we just use for example
+    * \return the result of a very complicated operation
+    */
+    double oneUsefulMethod(const OtherClass& argWellNamed);
+
+
+
+   protected:
+
+   private:
+    // copy constructor
+    MyClass(const MyClass&) = delete;
+    // copy assignment
+    MyClass& operator=(const MyClass&) = delete;
+    // move constructor
+    MyClass(MyClass&&) = delete;
+    // move assignment
+    MyClass& operator=(MyClass&&) = delete;
+
+    // Methods
+    int onePrivateMethod(int nbWaypoints);
+
+    // Private attributes
+
+    /** \brief Here is a nice attribut called _file and doing something */
+    int _nbFlightPlans {1};
+
+};
+}  // namespace MissionPlus
+}  // namespace Navblue
